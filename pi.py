e4d238ba584dcbc70be065427b108048dd63a58a
yuka/根本優花 <y.nemoto.24.4.1335@4nonome.com>
2024-04-21 09:19:56 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -5,3 +5,9 @@ text = """
 
 # TODO
 
+text = text.replace(",", "").replace(".", "")
+list_text = text.split()
+L_int = list(map(len,list_text))
+L = [str(A) for A in L_int]
+L = "".join(L)
+print(L)
