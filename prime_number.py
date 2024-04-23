c7a90a653be852152b7a48ef96734f73b05ff076
yuka/根本優花 <y.nemoto.24.4.1335@4nonome.com>
2024-04-23 17:55:51 +0900

    制御構文課題の解答

--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,22 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+#(1)
+import math
+n = 61
+def trial_division(n):
+   dest = int(math.sqrt(n)) + 1
+   
+   for i in range(2,dest):
+     if n%i == 0:
+        print(str(n) + 'は素数ではない')
+        return
+   print(str(n)+'は素数')
+    
+trial_division(n)
+
+
+#(2)
+
+n = 10
+trial_division(n)
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,18 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+import math
+a = 0
+b = math.pi/2
+n=int(100)
+
+dx=(b-a)/n
+s=0
+for i in range(0,n,1):
+    x1=a+dx*i
+    x2=a+dx*(i+1)
+    f1=math.sin(x1)
+    f2=math.sin(x2)
+    s=s+(f1+f2)*dx/2
+
+print("result=",s)
