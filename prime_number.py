36273b49219a824ace239775e2a36dbbb159e1c0
yuka/根本優花 <y.nemoto.24.4.1335@4nonome.com>
2024-04-25 11:42:19 +0900

    制御構文の解答ver.2

--- prime_number.py
+++ prime_number.py
@@ -6,18 +6,20 @@ b = input("bの値を入力: ")
 import math
 n = 61
 def trial_division(n):
-   dest = int(math.sqrt(n)) + 1
+      dest = int(math.sqrt(n)) + 1
    
-   for i in range(2,dest):
-     if n%i == 0:
-        print(str(n) + 'は素数ではない')
-        return
-   print(str(n)+'は素数')
+      if n <=1 :
+         print(str(n) + "は素数ではない")
+      else:
+         for i in range(2,dest):
+            if n%i == 0:
+               print(str(n) + 'は素数ではない')
+         return
+print(str(n)+'は素数')
     
 trial_division(n)
 
 
 #(2)
-
-n = 10
+n = 1
 trial_division(n)
