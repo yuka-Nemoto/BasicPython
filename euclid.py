afdfdb92e9e5a72f2894d39048f7ccfa76ffff3a
yuka/根本優花 <y.nemoto.24.4.1335@4nonome.com>
2024-04-30 00:32:13 +0900

    関数課題解答修正版ver.1

--- euclid.py
+++ euclid.py
@@ -2,3 +2,42 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+#問3
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b,a%b)
+
+#(1)
+a = 10
+b = 20
+print(gcd(a,b))
+
+#(2)
+a = 14
+b = 91
+print(gcd(a,b))
+
+#(3)
+a = 91
+b = 14
+print(gcd(a,b))
+
+#問4
+def judge(a,b):
+    return gcd(a,b) == 1
+#(1)
+a = 10
+b = 20
+print(judge(a,b))
+
+#(2)
+a = 14
+b = 91
+print(judge(a,b))
+
+#(3)
+a = 91
+b = 14
+print(judge(a,b))
--- prime_number.py
+++ prime_number.py
@@ -2,3 +2,19 @@ a = input("aの値を入力: ")
 b = input("bの値を入力: ")
 
 # TODO
+#(1)
+import math
+n = 61
+def trial_division(n):
+   dest = int(math.sqrt(n)) + 1
+   for i in range(2,dest):
+     if n%i == 0:
+        return False
+     else:
+        return True
+    
+print(trial_division(n))
+
+#(2)
+n = 10
+print(trial_division(n))
